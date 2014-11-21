ab077baea9fce4a63a1efe3e4290510b8a3eb70b
home <charly.palencia@koombea.com>
2014-11-21 08:20:17 -0500

    add food item

--- java/com/koombea/gamemin/items/ModItem.java
+++ java/com/koombea/gamemin/items/ModItem.java
@@ -1,12 +1,15 @@
 package com.koombea.gamemin.items;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemFood;
 
 public class ModItem {
 	public static Item orangeItem;
+	public static ItemFood itemOrangeFood;
 	
 	public static void init()
 	{
 		orangeItem = new ItemOrange();
+		itemOrangeFood = new ItemOrangeFood(4, 0.3f, false); //food points max:20, saturation , wolfmeat (raw beef) 
 	}
 }
